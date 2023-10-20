7b268488edacd15a07c281d5817e25ed3302be94
Armando Parra <ajp434@drexel.edu>
2023-10-20 00:51:56 -0400

    Game

---
+++ script.js
@@ -0,0 +1,282 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+
+let numbers = [];
+
+let countries = [
+  "Albania",
+  "Algeria",
+  "American Samoa",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cape Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cayman Islands",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Democratic Republic of the Congo",
+  "Republic of the Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czechia",
+  "Ivory Coast",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Guiana",
+  "French Polynesia",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "South Korea",
+  "North Korea",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Lao People's Democratic Republic",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Martinique",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Republic of North Macedonia",
+  "Romania",
+  "Russian Federation",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Sint Maarten",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syrian Arab Republic",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tokelau",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks and Caicos Islands",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom of Great Britain and Northern Ireland",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Venezuela",
+  "Vietnam",
+  "British Virgin Islands",
+  "Western Sahara",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+searchBtn.addEventListener("click", () => {
+  let number = countryInp.value;
+  if (!isNaN(number) && Number(number) > 0 && Number(number) < countries.length) {
+    let numbers = generateRan(Number(number))
+    result.innerHTML = "";
+    numbers.forEach(element => {
+      let finalURL = `https://restcountries.com/v3.1/name/${countries[element]}?fullText=true`;
+      fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+          const countryInfo = document.createElement("div");
+          countryInfo.classList.add("country-info");
+
+          const flagImage = document.createElement("img");
+          flagImage.src = data[0].flags.svg;
+          flagImage.classList.add("flag-img");
+          flagImage.addEventListener("click", () => {
+            toggleH2Visibility(flagImage);
+          });
+
+          const h2 = document.createElement("h2");
+          h2.textContent = data[0].name.common;
+
+          countryInfo.appendChild(flagImage);
+          countryInfo.appendChild(h2);
+
+          result.appendChild(countryInfo);
+        })
+        .catch(() => {
+          if (number.length == 0) {
+            result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+          }
+        });
+    })
+
+
+  } else {
+    result.innerHTML = `<h3>Please enter a different number.</h3>`;
+  }
+});
+
+function toggleH2Visibility(flagImage) {
+  const h2 = flagImage.nextElementSibling;
+  h2.style.display = h2.style.display === "none" ? "block" : "none";
+}
+
+function generateRan(max) {
+  var random = [];
+  for (var i = 0; i < max; i++) {
+    var temp = Math.floor(Math.random() * countries.length);
+    if (random.indexOf(temp) == -1) {
+      random.push(temp);
+    }
+    else
+      i--;
+  }
+  return random;
+}
